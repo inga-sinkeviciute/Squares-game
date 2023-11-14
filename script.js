1f7d975aa31d03a465a5cd07de5477740c17d6e6
Inga Sinkeviciute <isinkeviciute7@gmail.com>
2023-11-14 10:56:34 +0200

    additional changes

--- script.js
+++ script.js
@@ -3,32 +3,50 @@ let computerWins = 0;
 let round = 1;
 let timer;
 let playerName = "Player 1";
+let audio;
 
+// Event listener. Interactina su HTML, kai i inputa isivedu varda. Jei tuscia palieku, raso 'Player 1
 document
 	.getElementById("player-name-input")
 	.addEventListener("input", function () {
 		playerName = this.value || "Player 1";
 	});
 
+//Button click event. Klausosi 'click event'. Funckija suveikia paspaudus mygtuka. Tai paleidziu muzikele ir prasideda zaidimas.
 document.getElementById("start-button").onclick = function () {
 	startGame();
 	playAudio();
 };
 
 function startGame() {
+	// slepiam mygtuka po to kai paspaudi zaisti
 	const startButton = document.getElementById("start-button");
-	startButton.style.visibility = "hidden"; // Hide the button at the beginning
+	startButton.style.visibility = "hidden";
 
+	// player name input slepiam
 	document.getElementById("player-name-input").style.display = "none";
 
+	// rezultatus isvalom is buvusiu roundu
 	document.getElementById("result").innerText = "";
+
+	// 30s laikmatis ir roundu skaicius
 	document.getElementById("time").innerText = "30";
 	document.getElementById("round").innerText = round;
 
-	// Play audio when the game starts
-	const audio = new Audio("./audio/8bit-music-for-game-68698.mp3");
-	audio.play();
+	// muzikele groja
+	// Check if there is an existing audio instance
+	if (audio) {
+		if (!audio.paused) {
+			audio.pause(); // Stop the audio if it's playing
+		}
+		audio.currentTime = 0; // Rewind to the beginning
+	}
 
+	// Create a new audio instance
+	audio = new Audio("./audio/8bit-music-for-game-68698.mp3");
+	audio.play(); // Start the audio
+
+	// click event ant kvadrato perrasomas ir pajudinti square is vietos.
 	const square = document.getElementById("square");
 	square.onclick = function () {
 		playerWins++;
@@ -40,24 +58,30 @@ function startGame() {
 }
 
 function moveSquare() {
+	// kvadrato elementa paima
 	const square = document.getElementById("square");
+
+	// kad langelyje isitektu, neitu uz ribu
 	const maxX = window.innerWidth - square.clientWidth;
 	const maxY = window.innerHeight - square.clientHeight;
 
+	// sugeneruoja random kvadrato pozicijas
 	const randomX = Math.floor(Math.random() * maxX);
 	const randomY = Math.floor(Math.random() * maxY);
 
+	// random judejimas plius random spalva
 	square.style.left = `${randomX}px`;
 	square.style.top = `${randomY}px`;
 	square.style.backgroundColor = getRandomColor();
 
-	// Computer's move
+	// Computer's move. Ivedziau delay, nes kitu atveju labai jau mane aplosinejo :D
 	setTimeout(() => {
 		computerWins++;
 		moveSquare();
 	}, 30000);
 }
 
+// spalvos generatorius
 function getRandomColor() {
 	const letters = "0123456789ABCDEF";
 	let color = "#";
@@ -65,6 +89,7 @@ function getRandomColor() {
 		color += letters[Math.floor(Math.random() * 16)];
 	}
 
+	// priedejau dar border ir shadow, nes profesionaliau atrode
 	const borderStyle = `3px solid #8c6a6a`;
 	const boxShadowStyle = `3px 3px 5px #888888`;
 
@@ -76,10 +101,13 @@ function getRandomColor() {
 }
 
 function updateTimer() {
+	// time elementa per ID pasiimam
 	const timeElement = document.getElementById("time");
+
+	// current time value ir mazint
 	let time = parseInt(timeElement.innerText);
 	time--;
-
+	// patikrinam ar nuli pasieke
 	if (time === 0) {
 		clearInterval(timer);
 		endRound();
@@ -90,14 +118,19 @@ function updateTimer() {
 
 function endRound() {
 	const square = document.getElementById("square");
+
+	// Remove the click event handler from the square
 	square.onclick = null;
 
+	// Get the result element by its ID
 	const resultElement = document.getElementById("result");
+	// Display the result for the current round
 	resultElement.innerText = `Round ${round} ended! ${playerName}: ${playerWins}, Computer: ${computerWins}`;
 
 	// Hide the square at the end of the round
 	square.style.display = "none";
 
+	// Check if there are more rounds to play
 	if (round < 10) {
 		setTimeout(() => {
 			// Make the square visible again at the beginning of the new round
@@ -108,10 +141,11 @@ function endRound() {
 
 			round++;
 			startGame(); // Start a new round after a delay
-		}, 3000);
+		}, 5000);
 	} else {
 		// Game finished after 10 rounds
 		setTimeout(() => {
+			// Determine the winner based on the number of rounds won
 			if (playerWins > computerWins) {
 				resultElement.innerText = `Game Over! ${playerName} Wins - Rounds: ${playerWins}, Computer Wins: ${computerWins}`;
 			} else if (playerWins < computerWins) {
@@ -119,8 +153,9 @@ function endRound() {
 			} else {
 				resultElement.innerText = `Game Over! It's a Tie - Rounds: ${playerWins}, Computer Wins: ${computerWins}`;
 			}
+			// Make the start button visible
 			document.getElementById("start-button").style.display = "inline-block";
-		}, 3000);
+		}, 5000);
 	}
 }
 
